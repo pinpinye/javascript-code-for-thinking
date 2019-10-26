4475c6086ccc2f5f33ee5145d58a8cfb760af918
blinkpopy <569772668@qq.com>
2019-10-26 10:42:02 +0800

    Update javasript-prototype.js

--- javasript-prototype.js
+++ javasript-prototype.js
@@ -22,13 +22,14 @@ function extend(child, parent) {
   var prototype = new F(); // 拷贝原型
   prototype.constructor = child;
   child.prototype = prototype;
-  child.super = parent.prototype; // 这里是引用
+  child.super = parent.prototype; // 这里是对父级原型的引用
 }
 
- extend(Cat, Animal)
+extend(Cat, Animal)  // 在ES6中，这里封装的extend方法可以用 Object.setPrototypeOf(Cat.prototype, Animal.prototype)替代;
+
 Cat.prototype.sayName = function() {
   console.log("I am a cat");
-  Cat.super.sayName.call(this); // 通过引用调用，或者也可以直接通过Animal.prototype.sayName(this)调用
+  Cat.super.sayName.call(this); // 通过父级原型的引用调用，或者也可以直接通过Animal.prototype.sayName(this)调用
 }
 
 let myCat = new Cat('花花', 13);
