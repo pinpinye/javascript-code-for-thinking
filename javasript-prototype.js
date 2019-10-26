fe497ed687326d766cf6fab41d59f8c79e5190d7
blinkpopy <569772668@qq.com>
2019-10-26 11:51:50 +0800

    Update javasript-prototype.js

--- javasript-prototype.js
+++ javasript-prototype.js
@@ -16,7 +16,7 @@ function Cat(name, age) {
 }
 
 
-function extend(child, parent) {
+function extend(child, parent) {   // 道格拉斯，克罗克福德在prototypal inheritance in javascript一文中提出基于已有的的对象创建新对象 ，可以解决二次调用使用new父级构造函数的问题
   function F() {};
   F.prototype = parent.prototype;
   var prototype = new F(); // 拷贝原型
@@ -25,8 +25,12 @@ function extend(child, parent) {
   child.super = parent.prototype; // 这里是对父级原型的引用
 }
 
-extend(Cat, Animal)  // 在ES6中，这里封装的extend方法可以用 Object.setPrototypeOf(Cat.prototype, Animal.prototype)替代;
 
+// 在ES6中，这里封装的extend方法可以用 Object.setPrototypeOf(Cat.prototype, Animal.prototype)替代;
+// 如果考虑性能问题，那么应使用 child.prototype = Object.create(parent.prototype)
+extend(Cat, Animal)；
+
+// 不仅继承父类的同名方法。还可以实现子类定制的同名方法
 Cat.prototype.sayName = function() {
   console.log("I am a cat");
   Cat.super.sayName.call(this); // 通过父级原型的引用调用，或者也可以直接通过Animal.prototype.sayName(this)调用
@@ -36,10 +40,31 @@ let myCat = new Cat('花花', 13);
 myCat.sayName();
 myCat.sayAge();
 
+console.log(myCat instanceof Animal); // 打印出true
+console.log(Animal.prototype.isPrototypeOf(myCat)); // 打印出true
+console.log(myCat instanceof Cat); // 打印出true
+console.log(Cat.prototype.isPrototypeOf(myCat)); // 打印出true
+
 let myDog = new Animal('傻狗', 1);
                        
 myDog.sayName();
 myDog.sayAge();
+
+// 模拟new 操作符做的事情
+let creatChild = function(name, age) {
+  let child = {};
+  Cat.call(child,name,age); // 1. 调用指定父级的构造函数 
+  Object.setPrototypeOf(child, Cat.prototype); // 2. 添加一个__prtoto__指针指向指定父级的原型，setPrototypeOf 相当于 child.__prtoto__  = parent.ptototype
+  return child;
+}
+
+let myCat1 = creatChild('小花花', 12);  
+myCat1.sayName();
+myCat1.sayAge();
+console.log(myCat1 instanceof Animal); // 打印出true
+console.log(Animal.prototype.isPrototypeOf(myCat1)); // 打印出true
+console.log(myCat1 instanceof Cat); // 打印出true
+console.log(Cat.prototype.isPrototypeOf(myCat1)); // 打印出true
 
 
 
