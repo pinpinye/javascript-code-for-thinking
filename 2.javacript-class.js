3593a76d517b2b7fec0a02c40be7bfb88469c6f7
blinkpopy <569772668@qq.com>
2019-10-27 16:39:48 +0800

    Update 2.javacript-class.js

--- 2.javacript-class.js
+++ 2.javacript-class.js
@@ -1,1 +1,32 @@
 
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayName() {
+    console.log('I am ' + this.name);
+  }
+  sayAge() {
+    console.log('I am ' + this.age + 'years old');
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, color) {
+    super(name, age);
+    this.color = color;
+  }
+  sayName() {
+    console.log('I AM A CAT');
+    super.sayName();
+  }
+  sayColor() {
+    console.log('I look ' + this.color);
+  }
+}
+
+let mycat = new Cat('花花', 13, 'blue')
+mycat.sayName();
+mycat.sayAge();
+mycat.sayColor();
