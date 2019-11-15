6d0c92d49758f8a9641a9e0a1e63861f295a2160
blinkpopy <569772668@qq.com>
2019-11-15 20:33:10 +0800

    Update 4. javascript-deep-copy.js

--- 4. javascript-deep-copy.js
+++ 4. javascript-deep-copy.js
@@ -1,4 +1,5 @@
-
+// 方法一 递归
+// 考虑循环引用： 父级引用和同级引用
 function deepClone(o) {
   // let getType = (obj) => Object.prototype.toString.call(obj).split(" ")[1].slice(0,-1);
   if(o && typeof o === 'object' ) {
@@ -45,3 +46,10 @@ let c = {
 }
 c.copy = c
 console.log(deepClone(c))
+
+
+// .JSON.parse()和JSON.stringify()
+
+// undefined、任意的函数、正则表达式类型以及 symbol 值，在序列化过程中会被忽略（出现在非数组对象的属性值中时）或者被转换成 null（出现在数组中时)；
+// 它会抛弃对象的constructor。也就是深拷贝之后，不管这个对象原来的构造函数是什么，在深拷贝之后都会变成Object；
+// 如果对象中存在循环引用的情况无法正确处理。
